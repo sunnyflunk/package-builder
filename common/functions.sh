5353c2dd8964706740be6a9cb82c131ac703a883
Peter O'Connor <sunnyflunk@gmail.com>
2020-07-14 05:21:56 +1000

    Add initial debugging functions

---
+++ common/functions.sh
@@ -0,0 +1,32 @@
+#!/bin/true
+
+# Common functionality between all scripts
+
+
+# Emit a warning to tty
+function printWarning()
+{
+    echo -en '\e[1m\e[93m[WARNING]\e[0m '
+    echo $*
+}
+
+# Emit an error to tty
+function printError()
+{
+    echo -en '\e[1m\e[91m[ERROR]\e[0m '
+    echo $*
+}
+
+# Emit info to tty
+function printInfo()
+{
+    echo -en '\e[1m\e[94m[INFO]\e[0m '
+    echo $*
+}
+
+# Failed to do a thing. Exit fatally.
+function serpentFail()
+{
+    printError $*
+    exit 1
+}
