a9fdb503fa84ebbbdb846f548831c250403b80e9
Peter O'Connor <sunnyflunk@gmail.com>
2020-07-14 06:21:50 +1000

    Add zlib testfile to test build

---
+++ testfiles/zlib.sh
@@ -0,0 +1,10 @@
+#!/bin/true
+
+export ymlName=zlib
+export ymlVersion=1.2.11
+export ymlRelease=1
+export ymlLicense=ZLIB
+export ymlSources=( "https://github.com/madler/zlib/archive/v1.2.11.tar.gz" )
+export ymlSha256sums=( "629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff" )
+export ymlSummary="Zlib package"
+export ymlDescription="zlib is a general purpose data compression library. All the code is thread safe."
