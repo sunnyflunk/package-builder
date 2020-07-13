6a652b3b65e89b59707e5752581d1c69c2bb00d6
Peter O'Connor <sunnyflunk@gmail.com>
2020-07-14 05:10:28 +1000

    Create initial build variables to work off

---
+++ common/defaults-yml.sh
@@ -0,0 +1,37 @@
+#!/bin/true
+
+# Exporting default build variables
+export ymlName=
+export ymlVersion=
+export ymlRelease=
+export ymlLicense=
+export ymlSources=
+export ymlSha256sums=
+export ymlSummary=
+export ymlDescription=
+
+export buildClang=TRUE
+export buildDebug=FALSE
+export buildStrip=TRUE
+export buildCcache=FALSE
+export buildNetworking=FALSE
+
+export tunePerformance=FALSE
+export tuneOptimize=TRUE
+export tuneAsneeded=TRUE
+export tuneBindnow=TRUE
+export tuneSymbolic=TRUE
+export tuneRunpath=FALSE
+export tuneLdclean=TRUE
+export tuneLto=FALSE
+export tuneNoplt=TRUE
+export tuneMath=FALSE
+export tunehardened=FALSE
+export tuneSamplepgo=FALSE
+
+export stepEnvironment=
+export stepSetup=
+export stepBuild=
+export stepInstall=
+export stepCheck=
+export stepProfile=
