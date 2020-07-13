ea9f6cf06d3f84caf31d6e695af4262027345cd8
Peter O'Connor <sunnyflunk@gmail.com>
2020-07-14 06:22:21 +1000

    Add download functions to use

--- common/functions.sh
+++ common/functions.sh
@@ -30,3 +30,52 @@ function serpentFail()
     printError $*
     exit 1
 }
+
+# Verify the download is correct
+function verifyDownload()
+{
+    sourceURL="${1}"
+    sourceHash="${2}"
+    [ ! -z "${sourceURL}" ] || serpentFail "Missing URL for source: $1"
+    [ ! -z "${sourceHash}" ] || serpentFail "Missing hash for source: $1"
+    sourcePathBase=$(basename "${sourceURL}")
+    sourcePath="${PB_SOURCES_DIR}/${sourcePathBase}"
+
+    printInfo "Computing hash for ${sourcePathBase}"
+
+    computeHash=$(sha256sum "${sourcePath}" | cut -d ' ' -f 1)
+    [ $? -eq 0 ] || serpentFail "Failed to compute SHA256sum"
+
+    [ "${computeHash}" == "${sourceHash}" ] || serpentFail "Corrupt download: ${sourcePath}"
+}
+
+# Download a file from sources/
+function downloadSource()
+{
+    sourceURL="${1}"
+    sourceHash="${2}"
+    [ ! -z "${sourceURL}" ] || serpentFail "Missing URL for source: $1"
+    [ ! -z "${sourceHash}" ] || serpentFail "Missing hash for source: $1"
+    sourcePathBase=$(basename "${sourceURL}")
+    sourcePath="${PB_SOURCES_DIR}/${sourcePathBase}"
+
+    mkdir -p "${PB_SOURCES_DIR}" || serpentFail "Failed to create download tree"
+
+    if [[ -f "${sourcePath}" ]]; then
+        printInfo "Skipping download of ${sourcePathBase}"
+        return
+    fi
+
+    printInfo "Downloading ${sourcePathBase}"
+    curl -L --output "${sourcePath}" "${sourceURL}"
+    verifyDownload "${1}" "${2}"
+}
+
+# Extract a tarball into the current working directory
+function extractSource()
+{
+    [ ! -z "${1}" ] || serpentFail "Incorrect use of extractSource"
+    printInfo "Extracting ${sourcePathBase}"
+
+    tar xf "${1}" -C . || serpentFail "Failed to extract ${1}"
+}
