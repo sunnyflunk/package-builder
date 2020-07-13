31b23d9015bda4aa50de37fcc3e8fcf26c6dd515
Peter O'Connor <sunnyflunk@gmail.com>
2020-07-14 06:24:35 +1000

    Reuse variable names where possible

--- common/variables.sh
+++ common/variables.sh
@@ -2,9 +2,8 @@
 
 # Common directories to use
 
-PB_BUILD_DIR=/tmp/pb/${ymlName}
 PB_SOURCES_DIR=/tmp/pb/sources
-PB_WORKDIR=/tmp/pb/${ymlName}/workdir
-PB_INSTALLDIR=/tmp/pb/${ymlName}/install
-PB_PGO_DIR=/tmp/pb/${ymlName}/pgo
+PB_WORKDIR=${PB_BUILD_DIR}/workdir
+PB_INSTALLDIR=${PB_BUILD_DIR}/install
+PB_PGO_DIR=${PB_BUILD_DIR}/pgo
 PB_TESTFILES_DIR=${executionPath}/testfiles
