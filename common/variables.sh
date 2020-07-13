85c49fe92a41313da6f105093580c2bf691cdf61
Peter O'Connor <sunnyflunk@gmail.com>
2020-07-14 05:29:21 +1000

    Create initial build dir structure

---
+++ common/variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Common directories to use
+
+PB_BUILD_DIR=/tmp/pb/${ymlName}
+PB_SOURCES_DIR=/tmp/pb/sources
+PB_WORKDIR=/tmp/pb/${ymlName}/workdir
+PB_INSTALLDIR=/tmp/pb/${ymlName}/install
+PB_PGO_DIR=/tmp/pb/${ymlName}/pgo
+PB_TESTFILES_DIR=${executionPath}/testfiles
