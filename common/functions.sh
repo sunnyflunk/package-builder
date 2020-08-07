b3c48c9eb9bc25b3086f14ecf3e633bf619fc79c
Peter O'Connor <sunnyflunk@gmail.com>
2020-08-07 21:36:39 +1000

    Clean up steps into sensible functions

--- common/functions.sh
+++ common/functions.sh
@@ -135,3 +135,66 @@ function executeStep()
     echo "${@}"
     eval "${@}"
 }
+
+function debugAndstrip()
+{
+    elfFILES=$(find -type f -exec file {} \; | grep -i elf | cut -d: -f1)
+    for elf in $elfFILES; do
+        if [[ "$buildDebug" == true ]]; then
+            ${_OBJCOPY} --compress-debug-sections=zlib --only-keep-debug ${elf} ${elf}.debug
+            ${_OBJCOPY} --add-gnu-debuglink=${elf}.debug ${elf}
+            [[ -d ${PB_INSTALLDIR}/usr/lib/debug/$(dirname ${elf}.debug) ]] || mkdir -p ${PB_INSTALLDIR}/usr/lib/debug/$(dirname ${elf}.debug)
+            mv ${elf}.debug ${PB_INSTALLDIR}/usr/lib/debug/${elf}.debug
+        fi
+
+        if [[ "$buildStrip" == true ]]; then
+            ${_STRIP} -S --strip-unneeded --remove-section=.comment $elf
+        fi
+    done
+}
+
+function splitPkgs()
+{
+    find -type f,l -wholename *lib*/lib*.a | grep -v /lib32/ | sed 's|^./||' >> pkg-dev
+    find -type l -wholename *lib*/lib*.so | grep -v /lib32/ | sed 's|^./||' >> pkg-dev
+    find -type f,l -wholename */pkgconfig/*.pc | grep -v /lib32/ | sed 's|^./||' >> pkg-dev
+    find -type f,l -wholename */usr/include/* | sed 's|^./||' >> pkg-dev
+    find -type f,l -wholename */usr/share/aclocal* | sed 's|^./||' >> pkg-dev
+    find -type f,l -wholename *cmake/* | sed 's|^./||' >> pkg-dev
+    find -type f,l -wholename */usr/share/vala*/vapi/* | sed 's|^./||' >> pkg-dev
+    find -type f,l -wholename */usr/share/qt5/mkspecs/modules/*.pri | sed 's|^./||' >> pkg-dev
+    find -type f,l -wholename */usr/lib64/*.prl | sed 's|^./||' >> pkg-dev
+    find -type f,l -wholename */usr/share/doc/qt5/*.qch | sed 's|^./||' >> pkg-dev
+    find -type f,l -wholename */usr/share/doc/qt5/*.tags | sed 's|^./||' >> pkg-dev
+    find -type f,l -wholename */usr/lib32/lib*.so.* | grep -v [.]debug$ | sed 's|^./||' >> pkg-32
+    find -type f,l -wholename */usr/lib32/lib*.a | grep -v [.]debug$ | sed 's|^./||' >> pkg-32
+    find -type l -wholename */usr/lib32/lib*.so | sed 's|^./||' >> pkg-dev32
+    find -type f,l -wholename */usr/lib32/cmake/* | sed 's|^./||' >> pkg-dev32
+    find -type f,l -wholename */usr/lib32/pkgconfig/*.pc | sed 's|^./||' >> pkg-dev32
+    find -type f -name *.debug | grep -v /lib32/ | sed 's|^./||' >> pkg-dbg
+    find -type f,l -wholename */usr/lib32/* -name *.debug  | sed 's|^./||' >> pkg-dbg32
+}
+
+function makePkg()
+{
+    pkgSuffix=$1
+    if [[ ! -d ${ymlName}${pkgSuffix} ]]; then
+        mkdir -p ${ymlName}${pkgSuffix}
+        for file in $(cat pkg${pkgSuffix}); do
+            [[ -d ${ymlName}${pkgSuffix}/$(dirname $file) ]] || mkdir -p ${ymlName}${pkgSuffix}/$(dirname $file)
+            mv $file ${ymlName}${pkgSuffix}/$(dirname $file)/
+        done
+    fi
+    pushd ${ymlName}${pkgSuffix}
+        find -type f,l | grep -v files.yml$ | sed 's|^./||' | xargs sha256sum > files.yml
+        echo "Name: $ymlName${pkgSuffix}" > metadata.yml
+        echo "Version: $ymlVersion" >> metadata.yml
+        echo "Release: $ymlRelease" >> metadata.yml
+        echo "License: $ymlLicense" >> metadata.yml
+        echo "Summary: $ymlSummary" >> metadata.yml
+        echo "Description: $ymlDescription" >> metadata.yml
+        find -empty -type d -delete
+        tar --zstd -cf ../${ymlName}${pkgSuffix}-${ymlVersion}-${ymlRelease}.tar.zst *
+    popd
+    [[ -f pkg${pkgSuffix} ]] && rm pkg${pkgSuffix}
+}
--- pb.sh
+++ pb.sh
@@ -78,64 +78,12 @@ buildProcess
 
 printInfo "Examine and create the packages"
 pushd "${PB_INSTALLDIR}"
-    elfFILES=$(find -type f -exec file {} \; | grep -i elf | cut -d: -f1)
-    for elf in $elfFILES; do
-        if [[ "$buildDebug" == true ]]; then
-            ${_OBJCOPY} --compress-debug-sections=zlib --only-keep-debug ${elf} ${elf}.debug
-            ${_OBJCOPY} --add-gnu-debuglink=${elf}.debug ${elf}
-            [[ -d ${PB_INSTALLDIR}/usr/lib/debug/$(dirname ${elf}.debug) ]] || mkdir -p ${PB_INSTALLDIR}/usr/lib/debug/$(dirname ${elf}.debug)
-            mv ${elf}.debug ${PB_INSTALLDIR}/usr/lib/debug/${elf}.debug
-        fi
-
-        if [[ "$buildStrip" == true ]]; then
-            ${_STRIP} -S --strip-unneeded --remove-section=.comment $elf
-        fi
-    done
-
-    find -type f,l -wholename *lib*/lib*.a | grep -v /lib32/ | sed 's|^./||' >> pkg-dev
-    find -type l -wholename *lib*/lib*.so | grep -v /lib32/ | sed 's|^./||' >> pkg-dev
-    find -type f,l -wholename */pkgconfig/*.pc | grep -v /lib32/ | sed 's|^./||' >> pkg-dev
-    find -type f,l -wholename */usr/include/* | sed 's|^./||' >> pkg-dev
-    find -type f,l -wholename */usr/share/aclocal* | sed 's|^./||' >> pkg-dev
-    find -type f,l -wholename *cmake/* | sed 's|^./||' >> pkg-dev
-    find -type f,l -wholename */usr/share/vala*/vapi/* | sed 's|^./||' >> pkg-dev
-    find -type f,l -wholename */usr/share/qt5/mkspecs/modules/*.pri | sed 's|^./||' >> pkg-dev
-    find -type f,l -wholename */usr/lib64/*.prl | sed 's|^./||' >> pkg-dev
-    find -type f,l -wholename */usr/share/doc/qt5/*.qch | sed 's|^./||' >> pkg-dev
-    find -type f,l -wholename */usr/share/doc/qt5/*.tags | sed 's|^./||' >> pkg-dev
-    find -type f,l -wholename */usr/lib32/lib*.so.* | grep -v [.]debug$ | sed 's|^./||' >> pkg-32
-    find -type f,l -wholename */usr/lib32/lib*.a | grep -v [.]debug$ | sed 's|^./||' >> pkg-32
-    find -type l -wholename */usr/lib32/lib*.so | sed 's|^./||' >> pkg-dev32
-    find -type f,l -wholename */usr/lib32/cmake/* | sed 's|^./||' >> pkg-dev32
-    find -type f,l -wholename */usr/lib32/pkgconfig/*.pc | sed 's|^./||' >> pkg-dev32
-    find -type f -name *.debug | grep -v /lib32/ | sed 's|^./||' >> pkg-dbg
-    find -type f,l -wholename */usr/lib32/* -name *.debug  | sed 's|^./||' >> pkg-dbg32
+    debugAndstrip
 
     abireport scan-tree .
     cp ${PB_TESTFILES_DIR}/${1}.sh .
 
-    makePkg()
-    {
-        pkgSuffix=$1
-        if [[ ! -d ${ymlName}${pkgSuffix} ]]; then
-            mkdir -p ${ymlName}${pkgSuffix}
-            for file in $(cat pkg${pkgSuffix}); do
-                [[ -d ${ymlName}${pkgSuffix}/$(dirname $file) ]] || mkdir -p ${ymlName}${pkgSuffix}/$(dirname $file)
-                mv $file ${ymlName}${pkgSuffix}/$(dirname $file)/
-            done
-        fi
-        pushd ${ymlName}${pkgSuffix}
-            find -type f,l | grep -v files.yml$ | sed 's|^./||' | xargs sha256sum > files.yml
-            echo "Name: $ymlName${pkgSuffix}" > metadata.yml
-            echo "Version: $ymlVersion" >> metadata.yml
-            echo "Release: $ymlRelease" >> metadata.yml
-            echo "License: $ymlLicense" >> metadata.yml
-            echo "Summary: $ymlSummary" >> metadata.yml
-            echo "Description: $ymlDescription" >> metadata.yml
-            tar --zstd -cf ../${ymlName}${pkgSuffix}-${ymlVersion}-${ymlRelease}.tar.zst *
-        popd
-        [[ -f pkg${pkgSuffix} ]] && rm pkg${pkgSuffix}
-    }
+    splitPkgs
 
     [[ -f pkg-dev ]] && makePkg -dev
     [[ -f pkg-32 ]] && makePkg -32
